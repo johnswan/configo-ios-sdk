304d6b36a18ce826a8e79cfa7cfa329af3d8cff6
natanavra MBP15 <natanavra@gmail.com>
2015-12-20 20:16:47 +0200

    Changed copyright to Configo

--- ConfigoSDK/ConfigoSDK/Classes/Models/Response Header/CFGResponseHeader.m
+++ ConfigoSDK/ConfigoSDK/Classes/Models/Response Header/CFGResponseHeader.m
@@ -3,7 +3,7 @@
 //  ConfigoSDK
 //
 //  Created by Natan Abramov on 17/08/15.
-//  Copyright (c) 2015 Turrisio. All rights reserved.
+//  Copyright (c) 2015 Configo. All rights reserved.
 //
 
 #import "CFGResponseHeader.h"
